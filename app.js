ff5ac77c7f526358ccddfa2d36d6c40a7413af26
Robert Osipovitch <90695804+RobOsipo@users.noreply.github.com>
2022-03-05 11:01:08 -0500

    drop switch statement for toLocaleString

--- app.js
+++ app.js
@@ -11,42 +11,50 @@ app.get('/', (req, res) => {
     
     let today = new Date();
     let currentDay = today.getDay()
-    let day = ''
+    // let day = ''
 
-
-
-    switch (currentDay) {
-        case 0:
-            day = 'Sunday'
-            res.render('list', {typeOfDay: day})
-            break;
-        case 1:
-            day = 'Monday'
-            res.render('list', {typeOfDay: day})
-            break;
-        case 2: 
-            day = 'Tuesday'
-            res.render('list', {typeOfDay: day})
-            break;
-        case 3:
-            day = 'Wednesday'
-            res.render('list', {typeOfDay: day})
-            break;
-        case 4:
-            day = 'Thursday'
-            res.render('list', {typeOfDay: day})
-            break;
-        case 5:
-            day = 'Friday'
-            res.render('list', {typeOfDay: day})
-            break;
-        case 6:
-            day = 'Saturday'
-            res.render('list', {typeOfDay: day})
-            break;
-        default:
-            console.log('ERROR! Current Day is equal to ' + currentDay + ', it need to be 0 - 6')
+    let options = {
+        weekday: 'long', 
+        day: 'numeric',
+        month: 'long'
     }
+
+    let day = today.toLocaleDateString("en-US", options)
+
+    res.render('list', {typeOfDay: day})
+
+    // switch (currentDay) {
+    //     case 0:
+    //         day = 'Sunday'
+    //         res.render('list', {typeOfDay: day})
+    //         break;
+    //     case 1:
+    //         day = 'Monday'
+    //         res.render('list', {typeOfDay: day})
+    //         break;
+    //     case 2: 
+    //         day = 'Tuesday'
+    //         res.render('list', {typeOfDay: day})
+    //         break;
+    //     case 3:
+    //         day = 'Wednesday'
+    //         res.render('list', {typeOfDay: day})
+    //         break;
+    //     case 4:
+    //         day = 'Thursday'
+    //         res.render('list', {typeOfDay: day})
+    //         break;
+    //     case 5:
+    //         day = 'Friday'
+    //         res.render('list', {typeOfDay: day})
+    //         break;
+    //     case 6:
+    //         day = 'Saturday'
+    //         res.render('list', {typeOfDay: day})
+    //         break;
+    //     default:
+    //         console.log('ERROR! Current Day is equal to ' + currentDay + ', it need to be 0 - 6')
+    // }
 
 })
 
